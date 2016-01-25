d170eaaeba9d93b93771f380b1d571232617bd23
Jasmine Gulati <Sukhi_Gulati@rescomp-15-278446.stanford.edu>
2016-01-25 03:03:56 -0800

    ios simulator works

--- pauseapp/client/app.js
+++ pauseapp/client/app.js
@@ -1,1 +1,13 @@
 var app = angular.module('pauseApp', ['angular-meteor', 'ui.router']);
+
+function onReady() {
+	console.log ("on ready is called");
+	angular.bootstrap(document, ['pauseApp']);
+}
+
+if (Meteor.isCordova) {
+	angular.element(document).on("deviceready", onReady);
+}
+else {
+	angular.element(document).ready(onReady);
+}
