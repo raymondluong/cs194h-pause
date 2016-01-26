3fa79e554bdc183de653c3fdde0f78cddefd3f5a
Kyle Qian <cqian@stanford.edu>
2016-01-26 01:29:20 -0800

    added popover and audio

--- pauseapp/client/meditate.controller.js
+++ pauseapp/client/meditate.controller.js
@@ -2,8 +2,23 @@ angular.module('pauseApp').controller('MeditateCtrl', ['$scope', function ($scop
 	console.log('meditate controller');
 	
 	$scope.beginScan = function() {
+        
+        
 		$('.btn-body-scan').css('visibility', 'hidden');
-		scan();
+
+        var audioElement = document.createElement('audio');
+        // audioElement.setAttribute('src', 'meditation-voiceover.mp3');
+        audioElement.setAttribute('src', '2sec.mp3');
+        audioElement.setAttribute('autoplay', 'autoplay');
+        //audioElement.load();
+
+        audioElement.addEventListener("load", function() {
+            audioElement.play();
+        });
+
+        audioElement.addEventListener('ended', function(){
+            scan();
+        });
 	}
 
 	function scan() {
@@ -11,15 +26,15 @@ angular.module('pauseApp').controller('MeditateCtrl', ['$scope', function ($scop
         console.log('scan');
         if (scanner.length > 0) {
             scanner.css("visibility","visible").hide().fadeIn("slow", function() {
-                scanner.animate({top: "580px"}, 180000, "linear", function() {
+                scanner.animate({top: "620px"}, 180000, "linear", function() {
                     scanner.fadeOut("slow", function() {
                         scanner.show().css("visibility", "hidden");
                         setTimeout(function() {
 						 	window.location.href="home";
 						}, 1000);
                     });
-                })
-            })
+                });
+            });
         }
     }
 }]);
