77d325355bb75a1ea72f7f6e50a694ecc0ef7c36
julie <eiluj.ni@gmail.com>
2016-02-04 03:14:33 -0800

    edited color map

--- pauseapp/client/gradient.js
+++ pauseapp/client/gradient.js
@@ -1,5 +1,5 @@
 // Define variable colors
-	var back = ["#22A7F0","#8E44AD","#AEA8D3","#F62459","#DB0A5B","#D64541","#D2527F","#2C3E50","#1E8BC3","#87D37C","#4ECDC4","#3FC380","#E87E04","#F9690E","#F9BF3B"];
+	var moods = ["#22A7F0","#8E44AD","#AEA8D3","#F62459","#DB0A5B","#D64541","#D2527F","#2C3E50","#1E8BC3","#87D37C","#4ECDC4","#3FC380","#E87E04","#F9690E","#F9BF3B"];
 	
 	$('.gradient-bkgrd').each(function() {
 
@@ -18,12 +18,12 @@
 		}
 		
 		// Gradient rules
-		//grad.css('background-color', convertHex(back[0],40) );
-		//grad.css("background-image", "-webkit-gradient(linear, left top, left bottom, color-stop(0%,"+ convertHex(back[0],40) +"), color-stop(100%,"+ convertHex(back[1],40) +"))");
-		grad.css("background-image", "-webkit-linear-gradient(top, "+ convertHex(back[0],40) +","+ convertHex(back[1],40) +","+ convertHex(back[2],40) +","+ convertHex(back[3],40) +","+ convertHex(back[4],40) +","+ convertHex(back[5],40) +","+ convertHex(back[6],40) +","+ convertHex(back[7],40) +","+ convertHex(back[8],40) +","+ convertHex(back[9],40) +")");
-		grad.css("background-image", "-o-linear-gradient(top, "+ convertHex(back[0],40) +","+ convertHex(back[1],40) +","+ convertHex(back[2],40) +","+ convertHex(back[3],40) +","+ convertHex(back[4],40) +","+ convertHex(back[5],40) +","+ convertHex(back[6],40) +","+ convertHex(back[7],40) +","+ convertHex(back[8],40) +","+ convertHex(back[9],40) +")");
-		grad.css("background-image", "-ms-linear-gradient(top, "+ convertHex(back[0],40) +","+ convertHex(back[1],40) +","+ convertHex(back[2],40) +","+ convertHex(back[3],40) +","+ convertHex(back[4],40) +","+ convertHex(back[5],40) +","+ convertHex(back[6],40) +","+ convertHex(back[7],40) +","+ convertHex(back[8],40) +","+ convertHex(back[9],40) +")");
-		grad.css("background-image", "linear-gradient(to bottom, "+ convertHex(back[0],40) +","+ convertHex(back[1],40) +","+ convertHex(back[2],40) +","+ convertHex(back[3],40) +","+ convertHex(back[4],40) +","+ convertHex(back[5],40) +","+ convertHex(back[6],40) +","+ convertHex(back[7],40) +","+ convertHex(back[8],40) +","+ convertHex(back[9],40) +")");
-		//grad.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='"+ back[0] +"', endColorstr='"+ back[1] +"',GradientType=0 )");
+		//grad.css('background-color', convertHex(moods[0],40) );
+		//grad.css("background-image", "-webkit-gradient(linear, left top, left bottom, color-stop(0%,"+ convertHex(moods[0],40) +"), color-stop(100%,"+ convertHex(moods[1],40) +"))");
+		grad.css("background-image", "-webkit-linear-gradient(top, "+ convertHex(moods[0],40) +","+ convertHex(moods[1],40) +","+ convertHex(moods[2],40) +","+ convertHex(moods[3],40) +","+ convertHex(moods[4],40) +","+ convertHex(moods[5],40) +","+ convertHex(moods[6],40) +","+ convertHex(moods[7],40) +","+ convertHex(moods[8],40) +","+ convertHex(moods[9],40) +")");
+		grad.css("background-image", "-o-linear-gradient(top, "+ convertHex(moods[0],40) +","+ convertHex(moods[1],40) +","+ convertHex(moods[2],40) +","+ convertHex(moods[3],40) +","+ convertHex(moods[4],40) +","+ convertHex(moods[5],40) +","+ convertHex(moods[6],40) +","+ convertHex(moods[7],40) +","+ convertHex(moods[8],40) +","+ convertHex(moods[9],40) +")");
+		grad.css("background-image", "-ms-linear-gradient(top, "+ convertHex(moods[0],40) +","+ convertHex(moods[1],40) +","+ convertHex(moods[2],40) +","+ convertHex(moods[3],40) +","+ convertHex(moods[4],40) +","+ convertHex(moods[5],40) +","+ convertHex(moods[6],40) +","+ convertHex(moods[7],40) +","+ convertHex(moods[8],40) +","+ convertHex(moods[9],40) +")");
+		grad.css("background-image", "linear-gradient(to bottom, "+ convertHex(moods[0],40) +","+ convertHex(moods[1],40) +","+ convertHex(moods[2],40) +","+ convertHex(moods[3],40) +","+ convertHex(moods[4],40) +","+ convertHex(moods[5],40) +","+ convertHex(moods[6],40) +","+ convertHex(moods[7],40) +","+ convertHex(moods[8],40) +","+ convertHex(moods[9],40) +")");
+		//grad.css("filter", "progid:DXImageTransform.Microsoft.gradient( startColorstr='"+ moods[0] +"', endColorstr='"+ moods[1] +"',GradientType=0 )");
         
 	});
